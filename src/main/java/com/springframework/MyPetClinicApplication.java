7a0f27c4df72b193621cf6d90adfce7ea2e39962
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-13 14:55:38 -0700

    Rename package

--- src/main/java/com/springframework/MyPetClinicApplication.java
+++ src/main/java/com/springframework/MyPetClinicApplication.java
@@ -1,4 +1,4 @@
-package athar.springframework.mypetclinic;
+package com.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
