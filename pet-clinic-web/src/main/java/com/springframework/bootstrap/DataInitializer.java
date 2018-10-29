8a82839b20596c00bd85d622b519cd3f100ee1d1
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-29 14:02:01 -0700

    Modify Bootstrap class to add Owner and Pet properties

--- pet-clinic-web/src/main/java/com/springframework/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/springframework/bootstrap/DataInitializer.java
@@ -1,9 +1,12 @@
 package com.springframework.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.springframework.model.Owner;
+import com.springframework.model.Pet;
 import com.springframework.model.PetType;
 import com.springframework.model.Vet;
 import com.springframework.services.OwnerService;
@@ -33,16 +36,38 @@ public class DataInitializer implements CommandLineRunner{
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);	
-        System.out.println("Loaded Pet Types");
+        System.out.println("Pet Types Initialized");
         
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Dan");
 		owner1.setLastName("Brown");
+		owner1.setAddress("221 Baker Street");
+		owner1.setCity("London");
+		owner1.setTelephone("2218979");
+		
+		Pet DansPet = new Pet();
+		DansPet.setPetType(savedDogPetType);
+		DansPet.setOwner(owner1);
+		DansPet.setBirthDate(LocalDate.now());
+		DansPet.setName("Tiger");
+        owner1.getPets().add(DansPet);
+		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Jonathan");
 		owner2.setLastName("Nolan");
+		owner2.setAddress("333 Elm Street");
+		owner2.setCity("New York");
+		owner2.setTelephone("33446677");
+		
+		Pet JonathansPet = new Pet();
+		JonathansPet.setPetType(savedCatPetType);
+		JonathansPet.setOwner(owner2);
+		JonathansPet.setBirthDate(LocalDate.now());
+		JonathansPet.setName("Katzz");
+		owner2.getPets().add(JonathansPet);
+		
 		ownerService.save(owner2);
 		
 		System.out.println("Owners Initialized");
