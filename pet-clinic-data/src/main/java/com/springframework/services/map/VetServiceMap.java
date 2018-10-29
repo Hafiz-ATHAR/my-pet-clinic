a46fb7785ae6471b055765ba35ca8b9a1547a52d
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-29 15:39:22 -0700

    Add Specialities

--- pet-clinic-data/src/main/java/com/springframework/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/services/map/VetServiceMap.java
@@ -4,13 +4,22 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.springframework.model.Speciality;
 import com.springframework.model.Vet;
+import com.springframework.services.SpecialitiesService;
 import com.springframework.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
 
-	@Override
+	private final SpecialitiesService specilitiesService;
+	
+	
+	public VetServiceMap(SpecialitiesService specilitiesService) {
+	this.specilitiesService = specilitiesService;
+}
+	
+@Override
 	public Set<Vet> findAll() {
 		return super.findAll();
 	}
@@ -22,6 +31,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
 	@Override
 	public Vet save(Vet object) {
+		if(object.getSpecialities().size() >0) {
+			object.getSpecialities().forEach(speciality ->{
+				if(speciality.getId() == null) {
+					Speciality savedSpeciality = specilitiesService.save(speciality);
+					speciality.setId(savedSpeciality.getId());
+				}
+			});
+		}
 		return super.save(object);
 	}
 
--- pet-clinic-web/src/main/java/com/springframework/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/springframework/bootstrap/DataInitializer.java
@@ -2,15 +2,18 @@ package com.springframework.bootstrap;
 
 import java.time.LocalDate;
 
+import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.springframework.model.Owner;
 import com.springframework.model.Pet;
 import com.springframework.model.PetType;
+import com.springframework.model.Speciality;
 import com.springframework.model.Vet;
 import com.springframework.services.OwnerService;
 import com.springframework.services.PetTypeService;
+import com.springframework.services.SpecialitiesService;
 import com.springframework.services.VetService;
 
 @Component
@@ -19,72 +22,95 @@ public class DataInitializer implements CommandLineRunner{
 	private final OwnerService ownerService;
 	private final VetService vetService;
 	private final PetTypeService petTypeService;
-	
-	public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+	private final SpecialitiesService specialitiesService;
+
+	public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, 
+			SpecialitiesService specialitiesService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
+		this.specialitiesService = specialitiesService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		int count = petTypeService.findAll().size();
+		if (count == 0) {
+			loadData();
+		}
+	}
+
+	private	void loadData() {
 		PetType dog = new PetType();
 		dog.setName("Dog");
-        PetType savedDogPetType = petTypeService.save(dog);
-        
-        PetType cat = new PetType();
-        cat.setName("Cat");
-        PetType savedCatPetType = petTypeService.save(cat);	
-        System.out.println("Pet Types Initialized");
-        
+		PetType savedDogPetType = petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		PetType savedCatPetType = petTypeService.save(cat);	
+		System.out.println("Pet Types Initialized");
+
+		Speciality radiology = new Speciality();
+		radiology.setDescription("Radiology");
+		Speciality savedRadiology = specialitiesService.save(radiology);
+
+		Speciality surgery = new Speciality();
+		surgery.setDescription("Surgery");
+		Speciality savedSurgery = specialitiesService.save(surgery);
+
+		Speciality dentistry = new Speciality();
+		dentistry.setDescription("Dentistry");
+		Speciality savedDentistry = specialitiesService.save(dentistry);
+
+		System.out.println("Specialities Initialized");
+
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Dan");
 		owner1.setLastName("Brown");
 		owner1.setAddress("221 Baker Street");
 		owner1.setCity("London");
 		owner1.setTelephone("2218979");
-		
+
 		Pet DansPet = new Pet();
 		DansPet.setPetType(savedDogPetType);
 		DansPet.setOwner(owner1);
 		DansPet.setBirthDate(LocalDate.now());
 		DansPet.setName("Tiger");
-        owner1.getPets().add(DansPet);
-		
+		owner1.getPets().add(DansPet);
+
 		ownerService.save(owner1);
-		
+
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Jonathan");
 		owner2.setLastName("Nolan");
 		owner2.setAddress("333 Elm Street");
 		owner2.setCity("New York");
 		owner2.setTelephone("33446677");
-		
+
 		Pet JonathansPet = new Pet();
 		JonathansPet.setPetType(savedCatPetType);
 		JonathansPet.setOwner(owner2);
 		JonathansPet.setBirthDate(LocalDate.now());
 		JonathansPet.setName("Katzz");
 		owner2.getPets().add(JonathansPet);
-		
+
 		ownerService.save(owner2);
-		
+
 		System.out.println("Owners Initialized");
 
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Anthony");
 		vet1.setLastName("Hopkins");
-		
-		
+		vet1.getSpecialities().add(savedRadiology);
 		vetService.save(vet1);
-		
+
 		Vet vet2 = new Vet();
 		vet2.setFirstName("Johnny");
 		vet2.setLastName("Cash");
-		
+		vet2.getSpecialities().add(savedSurgery);
 		vetService.save(vet2);
-		
+
 		System.out.println("Vets Initialized");
 	}
 }
