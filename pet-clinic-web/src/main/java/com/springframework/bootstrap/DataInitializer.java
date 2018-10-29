89f8615492b3c311e40de21718b9118fa690b4dc
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-10-29 03:30:07 -0700

    Add Pet type data to Bootstrap data loader

--- pet-clinic-web/src/main/java/com/springframework/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/springframework/bootstrap/DataInitializer.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.springframework.model.Owner;
+import com.springframework.model.PetType;
 import com.springframework.model.Vet;
 import com.springframework.services.OwnerService;
+import com.springframework.services.PetTypeService;
 import com.springframework.services.VetService;
 
 @Component
@@ -13,25 +15,34 @@ public class DataInitializer implements CommandLineRunner{
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 	
-	public DataInitializer(OwnerService ownerService, VetService vetService) {
+	public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+        
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);	
+        System.out.println("Loaded Pet Types");
+        
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Dan");
 		owner1.setLastName("Brown");
-		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Jonathan");
 		owner2.setLastName("Nolan");
-		
 		ownerService.save(owner2);
 		
 		System.out.println("Owners Initialized");
